aaa1db058d43bde142430598f8728ccce6813606
Faraaz Khan <75445343+faraazkhan1112@users.noreply.github.com>
2020-12-05 18:21:16 +0530

    Create expenses.js

---
+++ backend/routes/expenses.js
@@ -0,0 +1,48 @@
+const router = require('express').Router();
+let Expense = require('../models/expense.model');
+
+router.route('/').get((req, res) => {
+  Expense.find()
+    .then(expenses => res.json(expenses))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/add').post((req, res) => {
+  const username = req.body.username;
+  const description = req.body.description;
+  const amount = Number(req.body.amount);
+  const date = Date.parse(req.body.date);
+
+  const newExpense = new Expense({username,description,amount,date});
+
+  newExpense.save()
+  .then(() => res.json('Expense added!'))
+  .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/:id').get((req, res) => {
+    Expense.findById(req.params.id)
+      .then(expense => res.json(expense))
+      .catch(err => res.status(400).json('Error: ' + err));
+  });
+  router.route('/:id').delete((req, res) => {
+    Expense.findByIdAndDelete(req.params.id)
+      .then(() => res.json('Expense deleted.'))
+      .catch(err => res.status(400).json('Error: ' + err));
+  });
+  router.route('/update/:id').post((req, res) => {
+    Expense.findById(req.params.id)
+      .then(expense => {
+        expense.username = req.body.username;
+        expense.description = req.body.description;
+        expense.amount = Number(req.body.amount);
+        expense.date = Date.parse(req.body.date);
+  
+        expense.save()
+          .then(() => res.json('Expense updated!'))
+          .catch(err => res.status(400).json('Error: ' + err));
+      })
+      .catch(err => res.status(400).json('Error: ' + err));
+  });
+
+module.exports = router;
