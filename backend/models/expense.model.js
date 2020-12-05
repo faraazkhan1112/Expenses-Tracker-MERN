7c42611f8ba7659bb6a864a32d18785ca9aa5ec0
Faraaz Khan <75445343+faraazkhan1112@users.noreply.github.com>
2020-12-05 18:19:49 +0530

    Create expense.model.js

---
+++ backend/models/expense.model.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const expenseSchema = new Schema({
+  username: { type: String, required: true },
+  description: { type: String, required: true },
+  amount: { type: Number, required: true },
+  date: { type: Date, required: true },
+}, {
+  timestamps: true,
+});
+
+const Expense = mongoose.model('Expense', expenseSchema);
+
+module.exports = Expense;
